735bc08b74cc9af977501099322f7ab18d2b734c
Just - 2Devs <67177611+Just2Devs@users.noreply.github.com>
2020-06-20 04:35:40 +0100

    Adicionadas mais traduções.

---
+++ locales/en.lua
@@ -0,0 +1,11 @@
+Locales['en'] = {
+	['shop'] = 'Loja Ilegal',
+	['shops'] = 'Lojas Ilegais',
+	['press_menu'] = 'Pressiona ~INPUT_CONTEXT~ para acessar a ~y~Loja~s~.',
+	['bought'] = 'Tu compraste ~y~%sx~s~ ~b~%s~s~ por ~r~€%s~s~',
+	['not_enough'] = 'Não tens dinheiro ~r~suficiente~s~ ~y~faltam te~s~ ~r~€%s~s~!',
+	['player_cannot_hold'] = 'Tu ~r~não~s~ tens ~y~espaço suficiente~s~ no teu inventário!',
+	['shop_confirm'] = 'Comprar %sx %s por €%s?',
+	['no'] = 'não',
+	['yes'] = 'sim',
+}
